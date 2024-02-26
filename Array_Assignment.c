c682af985e297a7eb4f7f8310846d5bee6438c0c
RiCo-OneilL <oneillrietchiey@gmail.com>
2024-02-26 22:47:39 +0300

    23rd Array assignment

---
+++ Array_Assignment.c
@@ -0,0 +1,35 @@
+/*RICHARD ABUTO
+  ENE212-0157/2021
+*/
+#include <stdio.h>
+int main()
+{
+    int Original[6]={2,3,5,7,9,0};
+       printf("Original Array\n");
+        for(int i=0; i<6 ; i++)
+    {
+        printf("%d,",Original[i]);
+    }
+    Original[5] = Original[4];
+    Original[4]= Original[3];
+    printf("\nEnter number to insert between 5 and 7 in the array\n");
+    scanf("%d",&Original[3]);
+    printf("New Array\n");
+    for(int i=0; i<6 ; i++)
+    {
+        printf("%d,",Original[i]);
+    }
+     Original[3]= Original[4];
+     Original[4]= Original[5];
+     Original[5]= 0;
+     printf("\nArray after deletion of the inserted number\n");
+
+     for(int i=0; i<6 ; i++)
+    {
+        printf("%d,",Original[i]);
+    }
+
+
+return 0;
+
+}
